0de9f5ab36a43ae4fee1e92b8dfa0a2c4acc7a87
FishTacoBidun <wolfie.essink@gmail.com>
2025-08-01 15:13:43 -0700

    Update essinkw_assignment4.c

--- essinkw_assignment4.c
+++ essinkw_assignment4.c
@@ -90,3 +90,91 @@ int main()
 
     return 0;
 }
+
+
+void toggleMode()
+{
+    if (ifBackground == true)
+    {
+        ifBackground = false;
+        printf("\nEntering foreground-only mode (& is now ignored)\n");
+    }
+    else
+    {
+        ifBackground = true;
+        printf("\nExiting foreground-only mode\n");
+    }
+}
+
+
+void dealWithSigtstp(int signo)
+{
+    if (ifForeground == false)
+    {
+        toggleMode();
+    }
+    else
+    {
+        ifSigtstp = true;
+    }
+    return;
+}
+
+
+void forget(int lineID, struct commandLine** processList)
+{
+    struct commandLine* curr = *processList;
+    struct commandLine* prev = NULL;
+
+    while (curr->lineID != lineID)
+    {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if (prev != NULL)
+    {
+        prev->next = curr->next;
+    }
+    else
+    {
+        *processList = curr->next;
+    }
+
+    free(curr);
+
+    return;
+}
+
+
+void processStatus(int lineID, struct commandLine** processList)
+{
+    int ifExited = -5;
+    int childExit = -5;
+    int statusValue = -5;
+    char status[MAX];
+
+    ifExited = waitpid(lineID, &childExit, WNOHANG);
+
+    if (ifExited == 0) 
+    {
+        return;
+    }
+
+    if (WIFEXITED(childExit) != 0)
+    {
+        statusValue = WEXITSTATUS(childExit);
+        printf(status, "background pid %d is done: exit value %d", lineID, statusValue);
+        printf("%s\n", status);
+        forget(lineID, processList);
+    }
+    else if (WIFSIGNALED(childExit) != 0)
+    {
+        statusValue = WTERMSIG(childExit);
+        printf(status, "background pid %d is done: terminated by signal %d", lineID, statusValue);
+        printf("%s\n", status);
+        forget(lineID, processList);
+    }
+
+    return;
+}
