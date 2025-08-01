92bb7f79dd12a259f4c9a28c6005021aa943ecf6
FishTacoBidun <wolfie.essink@gmail.com>
2025-08-01 14:03:17 -0700

    Update essinkw_assignment4.c

--- essinkw_assignment4.c
+++ essinkw_assignment4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
 
+//constants
+#define MAX 512
